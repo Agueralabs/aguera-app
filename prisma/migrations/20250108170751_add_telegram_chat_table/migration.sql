575b18b9ba893a9ec5178d1bf6d3c140b4d495ec
Auralex22 <joshkay2200@gmail.com>
2025-01-27 13:33:46 -0500

    Update migration.sql

--- prisma/migrations/20250108170751_add_telegram_chat_table/migration.sql
+++ prisma/migrations/20250108170751_add_telegram_chat_table/migration.sql
@@ -1,7 +1,7 @@
 /*
   Warnings:
 
-  - You are about to drop the column `telegramId` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `telegramId` on the `users` table. All the data in that column will be lost.
 
 */
 -- AlterTable
